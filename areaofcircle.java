497777bd4e98a32c149325688db038bf8c52520b
Priyadharshini345 <105904968+Priyadharshini345@users.noreply.github.com>
2022-05-20 12:16:36 +0530

    Add files via upload

---
+++ areaofcircle.java
@@ -0,0 +1,13 @@
+package assignment1;
+
+public class areaofcircle {
+
+	public static void main(String[] args) {
+		float r=4.27f;
+		float area= 0.14159f*r*r;
+		System.out.println("Area of circle with radius= "+r+" is "+area);
+		
+
+	}
+
+}
